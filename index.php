e09e238368a135d124a7efbf33a9dbe20fded134
13888285815 <87047954+13888285815@users.noreply.github.com>
2024-11-01 11:07:14 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @version        $Id: index.php 1 9:23 2010-11-11 tianya $
+ * @package        yndxw.Site
+ * @copyright      Copyright (c) 2018 - 2028, yndxw, Inc.
+ * @license        http://help.yndxw.com/usersguide/license.html
+ * @link           http://www.yndxw.com
+ */
+if(!file_exists(dirname(__FILE__).'/data/common.inc.php'))
+{
+    header('Location:install/index.php');
+    exit();
+}
+//自动生成HTML版
+if(isset($_GET['upcache']) || !file_exists('index.html'))
+{
+    require_once (dirname(__FILE__) . "/include/common.inc.php");
+    require_once DEDEINC."/arc.partview.class.php";
+    $GLOBALS['_arclistEnv'] = 'index';
+    $row = $dsql->GetOne("Select * From `#@__homepageset`");
+    $row['templet'] = MfTemplet($row['templet']);
+    $pv = new PartView();
+    $pv->SetTemplet($cfg_basedir . $cfg_templets_dir . "/" . $row['templet']);
+    $row['showmod'] = isset($row['showmod'])? $row['showmod'] : 0;
+    if ($row['showmod'] == 1)
+    {
+        $pv->SaveToHtml(dirname(__FILE__).'/index.html');
+        include(dirname(__FILE__).'/index.html');
+        exit();
+    } else { 
+        $pv->Display();
+        exit();
+    }
+}
+else
+{
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location:index.html');
+}
+?>
